c8a6e54ef15425e9cd81fe0d8aa2bf064c13ca09
SudharsonKR <sudharsonkr@gmail.com>
2023-12-05 20:33:32 +0530

    logout commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
     const logoutuser = async () => {
         let token = localStorage.getItem("usersdatatoken");
 
-        const res = await fetch("/logout", {
+        const res = await fetch("https://password-reset-dlwi.onrender.com/logout", {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
